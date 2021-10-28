c9f79b2b6bcb21a9895a158a70dadf9af37ceded
area268457913 <area268457913@hotmail.com>
2021-10-28 14:19:46 +0800

    feat: add mongoose

---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Record = require('../record')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})

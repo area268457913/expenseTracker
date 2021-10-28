dc9272611b6eb5e793cabdeda973985e7a8603da
area268457913 <area268457913@hotmail.com>
2021-10-28 14:38:12 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -15,7 +18,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000')
